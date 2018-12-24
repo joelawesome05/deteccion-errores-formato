8a480541b79b0f1eaf30b593a7d26c217ae66f9d
Joel <joelawesome05@hotmail.com>
2018-12-24 08:23:32 -0400

    Initial commit

---
+++ src/main/java/com/ucbcba/joel/deteccionerroresdeformato/DeteccionErroresDeFormatoApplication.java
@@ -0,0 +1,14 @@
+package com.ucbcba.joel.deteccionerroresdeformato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeteccionErroresDeFormatoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeteccionErroresDeFormatoApplication.class, args);
+	}
+
+}
+
